a443f1cfc682829d32b5300f13a86b02a4d890d3
Y-T04KA <70528231+Y-T04KA@users.noreply.github.com>
2021-11-17 22:19:52 +0300

    Update aisd-avl.cpp

--- aisd-avl.cpp
+++ aisd-avl.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <queue>
 
 using namespace std;
 
@@ -60,7 +59,7 @@ int getBalance(Node* N) {
     return height(N->left) - height(N->right);
 }
 
-Node* insert(Node* node, int key) {//а это
+Node* insert(Node* node, int key) {//Г  ГЅГІГ®
     if (node == NULL) return(newNode(key));
     if (key < node->key) node->left = insert(node->left, key);
     else if (key > node->key) node->right = insert(node->right, key);
@@ -184,7 +183,7 @@ Node* minValueNode(Node* node) {
     return current;
 }
 
-Node* deleteNode(Node* root, int key) {//а поиск ноды для удаления считается за поиск?
+Node* deleteNode(Node* root, int key) {//Г  ГЇГ®ГЁГ±ГЄ Г­Г®Г¤Г» Г¤Г«Гї ГіГ¤Г Г«ГҐГ­ГЁГї Г±Г·ГЁГІГ ГҐГІГ±Гї Г§Г  ГЇГ®ГЁГ±ГЄ?
     if (root == NULL) return root;
     if (key < root->key) root->left = deleteNode(root->left, key);
     else if (key > root->key) root->right = deleteNode(root->right, key);
